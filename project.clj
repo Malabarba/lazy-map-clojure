d9d0520ba49131d819d2e007347044133be19f37
Artur Malabarba <bruce.connor.am@gmail.com>
2015-06-25 02:10:57 +0100

    0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.malabarba/lazy-map "0.1.0"
+(defproject org.clojars.malabarba/lazy-map "0.1.2"
   :description "Create lazy maps from data or from java objects. Entry
 points are the `core/lazy-map` and the `iop/extend-lazy-map` macros."
   :url "https://github.com/Malabarba/lazy-map-clojure"
