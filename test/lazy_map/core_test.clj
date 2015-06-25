8ec6cac154320c65b888581f8c94102affaa3409
Artur Malabarba <bruce.connor.am@gmail.com>
2015-06-25 01:09:37 +0100

    Define tests

--- test/lazy_map/core_test.clj
+++ test/lazy_map/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [lazy-map.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest lazy
+  (let [a (atom 0)
+        m (lazy-map {:a (swap! a inc)})]
+    (is (= 0 @a))
+    (is (= 1 (:a m)))
+    (is (= 1 @a))
+    (is (= 1 (:a m)))
+    (is (= 1 @a))))
+
+(deftest safe
+  (let [sym (gensym "!")
+        m (lazy-map {:a nil :b "ok" :c 1 :d true
+                     :e sym})]
+    (are [k v] (= (k m) v)
+      :a nil :b "ok" :c 1 :d true :e sym))
+  (let [sym (gensym "!")
+        m (lazy-map.core.LazyMap {:a nil :b "ok" :c 1 :d true
+                                  :e sym})]
+    (are [k v] (= (k m) v)
+      :a nil :b "ok" :c 1 :d true :e sym)))
---
+++ test/lazy_map/iop_test.clj
@@ -0,0 +1,39 @@
+(ns lazy-map.iop-test
+  (:require [lazy-map.iop :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest a-test
+  (extend-lazy-map String)
+  (extend-lazy-map Exception)
+  (let [s (to-lazy-map "The String")
+        e (to-lazy-map (Exception. "The Exception"))]
+    (is (= (count s) 14))
+    (is (= (count e) 10))
+    (is (= (:length s) 10))
+    (is (not (:length e)))
+    (is (not (:notify-all e)))))
+
+(defn ex [] (Exception. "The Exception"))
+
+(deftest b-test
+  (extend-lazy-map String
+                   :exclude [toString]
+                   :post-fns {length dec})
+  (extend-lazy-map Exception
+                   :allow-impure true)
+  (let [s (to-lazy-map "The String")
+        e (to-lazy-map (ex))]
+    (is (= (count s) 13))
+    (is (= (count e) 10))
+    (is (= (:length s) 9))
+    (is (try (:notify-all e)
+             (catch Exception e true)))))
+
+(deftest c-test
+  (extend-lazy-map Exception
+                   :allow-impure true
+                   :catch-exceptions true
+                   :keyname {toString :boogie})
+  (is (not (:notify-all (to-lazy-map (ex)))))
+  (is (= (:boogie (to-lazy-map (ex))) "java.lang.Exception: The Exception")))
