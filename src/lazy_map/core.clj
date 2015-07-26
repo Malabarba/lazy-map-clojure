f61e260b11d30382e0bd76f3827e5481c2a6be9f
Artur Malabarba <bruce.connor.am@gmail.com>
2015-07-26 19:01:55 +0100

    more more dummy

--- src/lazy_map/core.clj
+++ src/lazy_map/core.clj
@@ -95,3 +95,65 @@
   [m]
   `(LazyMap.
     ~(into {} (map (fn [[k v]] [k `(lazy-val ~v)]) m))))
+(deftype LazyMap [contents]
+  clojure.lang.IPersistentMap
+  (assoc [_ k v]
+    (LazyMap. (.assoc contents k v)))
+  (assocEx [_ k v]
+    (LazyMap. (.assocEx contents k v)))
+  (without [_ k]
+    (LazyMap. (.without contents k)))
+
+  java.lang.Iterable
+  (iterator [this]
+    (.iterator (into {} (map (fn [[k v]] [k (getv v)]) contents))))
+
+  clojure.lang.Associative
+  (containsKey [_ k]
+    (.containsKey contents k))
+  (entryAt [_ k]
+    (getv (.entryAt contents k)))
+
+  clojure.lang.IPersistentCollection
+  (count [_] (.count contents))
+  (empty [_] (.empty contents))
+  (cons [_ o]
+    (LazyMap. (.cons contents o)))
+  (equiv [_ o]
+    (and (isa? (class o) LazyMap)
+         (.equiv contents (.contents o))))
+
+  clojure.lang.Seqable
+  (seq [_] (.seq contents))
+  
+  clojure.lang.ILookup
+  (valAt [_ k]
+    (getv (.valAt contents k)))
+  (valAt [_ k not-found]
+    (getv (.valAt contents k not-found))))
+
+
+;;; Map creation
+(defmacro lazy-map
+  "Return a LazyMap created from a map `m`.
+  The values in `m` are only evaluated when accessed.
+
+  ```clojure
+  user> (def my-map
+          (lazy-map
+           {:cause (do (println \"Getting Cause\")
+                       :major-failure)
+            :name (do (println \"Getting Name\")
+                      \"Some Name\")}))
+  #'user/my-map
+
+  user> (:name my-map)
+  Getting Name
+  \"Some Name\"
+
+  user> (:name my-map)
+  \"Some Name\"
+  ```"
+  [m]
+  `(LazyMap.
+    ~(into {} (map (fn [[k v]] [k `(lazy-val ~v)]) m))))
