67e3a1812188068005586deb35322e0bc016816e
Artur Malabarba <bruce.connor.am@gmail.com>
2015-09-07 22:25:17 +0100

    Add scm data

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.malabarba/lazy-map "0.1.2"
+(defproject org.clojars.malabarba/lazy-map "0.1.4"
   :description "Create lazy maps from data or from java objects. Entry
 points are the `core/lazy-map` and the `iop/extend-lazy-map` macros."
   :url "https://github.com/Malabarba/lazy-map-clojure"
@@ -6,6 +6,8 @@ points are the `core/lazy-map` and the `iop/extend-lazy-map` macros."
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.8.12"]]
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :scm {:name "git"
+        :url "https://github.com/Malabarba/lazy-map-clojure"}
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "https://github.com/Malabarba/lazy-map-clojure/blob/master/"
           :src-linenum-anchor-prefix "L"})
