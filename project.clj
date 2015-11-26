6c2eaa16c241d778488f9fa7ca55d0c9fb855ca5
Stuart Mitchell <stu@stuartmitchell.com>
2015-11-26 17:20:56 +1300

    Fixed travis and project files

--- project.clj
+++ project.clj
@@ -5,32 +5,41 @@ points are the `core/lazy-map` and the `iop/extend-lazy-map` macros."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.8.12"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :scm {:name "git"
         :url "https://github.com/Malabarba/lazy-map-clojure"}
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "https://github.com/Malabarba/lazy-map-clojure/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
 
-             :test {:resource-paths ["test/resources"]}
-             :test-clj {:test-paths ["test/clj"]}
-             ;; :test-cljs {:test-paths ["test/cljs"]
-             ;;             :dependencies [[com.cemerick/piggieback "0.2.1"]
-             ;;                            [org.clojure/clojurescript "0.0-3211"]]}
-
+  :profiles {:test {:test-paths ["test/cljc"]}
+           :test-cljs {:dependencies [[org.clojure/clojurescript "1.7.170"]]
+                       :plugins [[lein-cljsbuild   "1.1.1"]
+                                 [lein-doo "0.1.6-rc.1"]]}
              :coveralls {:plugins [[lein-cloverage "1.0.2"]
                                    [lein-shell "0.4.0"]]
                          :aliases {"coveralls" ["do" "cloverage" "--coveralls,"
                                                 "shell" "curl" "-F"
                                                 "json_file=@target/coverage/coveralls.json"
                                                 "https://coveralls.io/api/v1/jobs"]}}
-
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-
              :cljfmt {:plugins [[lein-cljfmt "0.3.0"]]
                       :cljfmt {:indents {as-> [[:inner 0]]}}}
              :eastwood {:plugins [[jonase/eastwood "0.2.1"]]
-                        :eastwood {:config-files ["eastwood.clj"]}}})
+                        :eastwood {:config-files ["eastwood.clj"]}}}
+  :cljsbuild
+  {:builds
+   [{:id "main"
+     :source-paths   ["src"]
+     ;; :notify-command ["terminal-notifier" "-title" "cljsbuild" "-message"]
+     :compiler       {:output-to "resources/public/js/testable.js"
+                      :main lazy-map.test-runner
+                      :optimizations :advanced}}
+    {:id "test"
+     :source-paths   ["src" "test"]
+     :compiler       {:output-to "resources/public/js/testable.js"
+                      :main lazy-map.test-runner
+                      :optimizations :none}}]}
+  :aliases {"clj-test" ["with-profile" "+test" "test"]
+            "cljs-test" ["with-profile" "+test-cljs" "do"
+                          ["clean"]
+                          ["doo" "phantom" "test" "auto"]]})
